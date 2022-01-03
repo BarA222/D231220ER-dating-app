befc6e90914b1d7a31fea4e4a53ec376f8764d9d
BarAz <barazriel2@gmail.com>
2022-01-03 11:21:36 +0200

    End of section 4

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,7 @@ namespace API.Controllers
           _context= context;
         }      
        [HttpGet]
+       [AllowAnonymous]
        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsersAsync()
        {
            var users = await _context.Users.ToListAsync();
@@ -24,6 +26,7 @@ namespace API.Controllers
        }
 
         //Get user by specific id --> api/users/3
+        [Authorize]
        [HttpGet("{id}")]
           public async Task<ActionResult<AppUser>> GetUserAsync(int id)
        {
